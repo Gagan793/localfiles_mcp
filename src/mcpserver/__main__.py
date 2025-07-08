5b94fcc3041071dfa07245806e796b0422cf0cd0
Gagan Gupta <114746231+Gagan793@users.noreply.github.com>
2025-07-08 16:48:43 +0530

    Update __main__.py

--- src/mcpserver/__main__.py
+++ src/mcpserver/__main__.py
@@ -1,9 +1,9 @@
 from mcpserver.local import mcp
-from mcpserver.prompt import mcp
+from mcpserver.prompt import mcpserver
 def main():
     # Optional: change host/port if you want it remotely accessible
     mcp.run()
-    mcp.run()
+    mcpserver.run()
     # mcp.run(host="0.0.0.0", port=8081)
 
 if __name__ == "__main__":
